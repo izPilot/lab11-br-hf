65d31d13f7a321a37ff07294fc28ebd5a9ef3c4e
izPilot <wolfhowljr@gmail.com>
2026-04-12 16:17:49 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,35 @@
+# https://github.com/izPilot/lab11-br-hf
+# Partner 1: Braden Rennolds
+# Partner 2: Hunter Fairbanks
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+    return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Denominator 'a' cannot be zero.")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Logarithm base 'a' must be greater than 0 and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Logarithm argument 'b' must be greater than 0.")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
