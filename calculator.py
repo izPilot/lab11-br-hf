816324744cac99640f51cd5aadbe6dc8cd1d5a38
izPilot <wolfhowljr@gmail.com>
2026-04-12 21:16:17 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -8,28 +8,3 @@ calculator.py
 
 One function per operation, in order.
 """
-import math
-
-def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Division by zero is not allowed.")
-    return b / a
-
-def log(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError("Logarithm base 'a' must be greater than 0 and not equal to 1.")
-    if b <= 0:
-        raise ValueError("Logarithm argument 'b' must be strictly positive.")
-    return math.log(b, a)
-
-def exp(a, b):
-    return a ** b
