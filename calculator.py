77d5afc40ae0a5d91e4d7af47e6daf6ab3c6277d
izPilot <wolfhowljr@gmail.com>
2026-04-12 21:11:18 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # https://github.com/izPilot/lab11-br-hf
-# Partner 1: Braden Rennolds
-# Partner 2: Hunter Fairbanks
+# Partner 1: Hunter Fairbanks
+# Partner 2: Braden Rennolds
 
 """
 calculator.py
@@ -13,23 +13,23 @@ import math
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
-        raise ZeroDivisionError("Denominator 'a' cannot be zero.")
+        raise ZeroDivisionError("Division by zero is not allowed.")
     return b / a
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or a == 1:
         raise ValueError("Logarithm base 'a' must be greater than 0 and not equal to 1.")
     if b <= 0:
-        raise ValueError("Logarithm argument 'b' must be greater than 0.")
+        raise ValueError("Logarithm argument 'b' must be strictly positive.")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
