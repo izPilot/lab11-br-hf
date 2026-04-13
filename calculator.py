bbda9d658645ab32c9023b618187f3ebb3e5ca3d
izPilot <107336893+izPilot@users.noreply.github.com>
2026-04-13 11:33:42 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # https://github.com/izPilot/lab11-br-hf
-# Partner 1: Hunter Fairbanks
-# Partner 2: Braden Rennolds
+# Partner 1: Braden Rennolds
+# Partner 2: Hunter Fairbanks
 
 """
 calculator.py
