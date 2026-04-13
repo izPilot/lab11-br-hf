f52df9f1af64c638a1952e2d5189d7f24f7e3550
izPilot <wolfhowljr@gmail.com>
2026-04-12 21:20:35 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -14,23 +14,23 @@ import math
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
-        raise ZeroDivisionError("Denominator 'a' cannot be zero.")
+        raise ZeroDivisionError("Division by zero is not allowed.")
     return b / a
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or a == 1:
         raise ValueError("Logarithm base 'a' must be greater than 0 and not equal to 1.")
     if b <= 0:
-        raise ValueError("Logarithm argument 'b' must be greater than 0.")
+        raise ValueError("Logarithm argument 'b' must be strictly positive.")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
