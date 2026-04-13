2fa180206946d617834a0c62d130f070b8154618
izPilot <wolfhowljr@gmail.com>
2026-04-12 22:42:10 -0400

    partner1 functions completed

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,10 @@
 # https://github.com/izPilot/lab11-br-hf
-# Partner 1: Hunter Fairbanks
-# Partner 2: Braden Rennolds
+# Partner 1: Braden Rennolds
+# Partner 2: Hunter Fairbanks
 
 import unittest
 from calculator import *
+
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -15,11 +16,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):  # 3 assertions
+        self.assertEqual(mul(10, 5), 50)
+        self.assertEqual(mul(-2, 4), -8)
+        self.assertEqual(mul(0, 100), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self):  # 3 assertions
+        self.assertEqual(div(4, 20), 5.0)
+        self.assertEqual(div(2, -10), -5.0)
+        self.assertAlmostEqual(div(2, 5), 2.5)
+
     # ##########################
 
     ######## Partner 2
@@ -36,24 +42,23 @@ class TestCalculator(unittest.TestCase):
     #     # use same technique from test_divide_by_zero
     #     fill in code
     # ##########################
-    
+
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):  # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(5, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):  # 3 assertions
+        self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
+        self.assertAlmostEqual(hypotenuse(1.5, 2.0), 2.5)
+        self.assertAlmostEqual(hypotenuse(-5, 12), 13.0)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self):  # 3 assertions
+        self.assertIsNone(square_root(-1))
+        self.assertEqual(square_root(25), 5.0)
+        self.assertAlmostEqual(square_root(2.25), 1.5)
     ##########################
+
 
 # Do not touch this
 if __name__ == "__main__":
